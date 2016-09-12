0444238b912a812a61939b46e6182767c6d55a8c
McDanci <mcdanci@users.noreply.github.com>
2016-09-12 10:24:06 +0800

    Rename files

--- php-g11n-gettext/g11n_gettext.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace mcdanci\execise\g11n;
-// reference: https://blog.longwin.com.tw/2007/09/gettext_php_i18n_2007/
-
-define('LC_SPOKEN', 0);
-define('LC_WRITTEN', 1);
-
-class gettext
-{}
---
+++ php_g11n_gettext/g11n_gettext.php
@@ -0,0 +1,9 @@
+<?php
+namespace mcdanci\execise\php_g11n_gettext;
+
+// reference: https://blog.longwin.com.tw/2007/09/gettext_php_i18n_2007/
+define('LC_SPOKEN', 0);
+define('LC_WRITTEN', 1);
+
+class gettext
+{}
